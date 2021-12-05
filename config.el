cce67f344bc8eff161b59c9a6267957c7ecc2a41
MoritzBoehme <mr.x@moritzboeh.me>
2021-12-05 21:11:26 +0100

    change dictionary location

--- config.el
+++ config.el
@@ -54,7 +54,7 @@
   (ispell-hunspell-add-multi-dic "de_DE,en_GB,en_US")
   ;; For saving words to the personal dictionary, don't infer it from
   ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
-  (setq ispell-personal-dictionary "~/.hunspell_personal"))
+  (setq ispell-personal-dictionary "~/.config/doom/.dictionary.txt"))
 
 (unless (file-exists-p ispell-personal-dictionary)
   (write-region "" nil ispell-personal-dictionary nil 0))
